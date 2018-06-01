756744d007b8a8d59ca33123e285f9f76afff940
wskiawv <wskiawv@163.com>
2018-06-01 16:50:31 +0800

    修改页面，添加国际化，去掉多余中文

--- src/main/java/com/my/blog/website/utils/TaleUtils.java
+++ src/main/java/com/my/blog/website/utils/TaleUtils.java
@@ -182,22 +182,24 @@ public class TaleUtils {
      * @return
      */
     public static DataSource getNewDataSource() {
-        if (newDataSource == null) synchronized (TaleUtils.class) {
-            if (newDataSource == null) {
-                Properties properties = TaleUtils.getPropFromFile("application-jdbc.properties");
-                if (properties.size() == 0) {
-                    return newDataSource;
-                }
-                DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
-                //        TODO 对不同数据库支持
-                managerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-                managerDataSource.setPassword(properties.getProperty("spring.datasource.password"));
-                String str = "jdbc:mysql://" + properties.getProperty("spring.datasource.url") + "/" + properties.getProperty("spring.datasource.dbname") + "?useUnicode=true&characterEncoding=utf-8&useSSL=false";
-                managerDataSource.setUrl(str);
-                managerDataSource.setUsername(properties.getProperty("spring.datasource.username"));
-                newDataSource = managerDataSource;
-            }
-        }
+        if (newDataSource == null) {
+			synchronized (TaleUtils.class) {
+			    if (newDataSource == null) {
+			        Properties properties = TaleUtils.getPropFromFile("application-jdbc.properties");
+			        if (properties.size() == 0) {
+			            return newDataSource;
+			        }
+			        DriverManagerDataSource managerDataSource = new DriverManagerDataSource();
+			        //        TODO 对不同数据库支持
+			        managerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+			        managerDataSource.setPassword(properties.getProperty("spring.datasource.password"));
+			        String str = "jdbc:mysql://" + properties.getProperty("spring.datasource.url") + "/" + properties.getProperty("spring.datasource.dbname") + "?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+			        managerDataSource.setUrl(str);
+			        managerDataSource.setUsername(properties.getProperty("spring.datasource.username"));
+			        newDataSource = managerDataSource;
+			    }
+			}
+		}
         return newDataSource;
     }
 
